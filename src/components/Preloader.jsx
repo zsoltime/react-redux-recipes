e020618b32a9514ae2c64cd0c88d4db3af5f6379
Zsolt Meszaros <zsolt@sowad.com>
2017-08-29 17:03:13 +0100

    Add preloader

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Preloader = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="preloader"
+    viewBox="0 0 100 32"
+    width="100"
+    height="32"
+  >
+    <circle cx="25" cy="16" r="8" />
+    <circle cx="50" cy="16" r="8" />
+    <circle cx="75" cy="16" r="8" />
+  </svg>
+);
+
+export default Preloader;
